f87c3dd5be24b27e564d81a636e55c7f8670d181
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2024-05-29 10:41:22 -0700

    Update SlideEncoding.py
    - cleaned up

--- SlideEncoding.py
+++ SlideEncoding.py
@@ -47,20 +47,3 @@ def encode_tiles(patient_id, tile_path, result_path, device='cpu'):
         hdf.create_dataset('mag', data=read["magnification"])
         hdf.create_dataset('size', data=read["size"])
         features_dataset = hdf.create_dataset('features', data=torch.stack(total_data))
-
-    # # create hdf5 file
-    # for i, row in read.iterrows():
-    #     # get individual tiles
-    #     path_to_tile = row["path_to_slide"]
-    #     try:
-    #         tile = Image.open(path_to_tile)
-    #         if tile is not None:
-    #             tile = preprocess(tile).unsqueeze(0).to(device)
-    #             with torch.no_grad():
-    #                 encoded_features = encoder_model(tile)
-    #             patient_tiles[path_to_tile] = encoded_features.cpu()  # Move tensor to CPU before saving
-    #     except Exception as e:
-    #         print(f"Error processing tile {path_to_tile}: {e}")
-    # patient_results = os.path.join(result_path, f"{patient_id}.pkl")
-    # with open(patient_results, "wb") as f:
-    #     pickle.dump(patient_tiles, f)
