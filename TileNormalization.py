e013bf11c22dfd14f74bbdf4df6daa10b0b7764f
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2024-10-23 12:04:34 -0700

    Update TileNormalization.py

--- TileNormalization.py
+++ TileNormalization.py
@@ -2,22 +2,23 @@ import numpy as np
 from PIL import Image
 
 
-def normalizeStaining(img, saveFile=None, Io=240, alpha=1, beta=0.15):
+def normalizeStaining(tile, Io=240, alpha=1, beta=0.15):
     try:
         HERef = np.array([[0.5626, 0.2159],
                           [0.7201, 0.8012],
                           [0.4062, 0.5581]])
 
         maxCRef = np.array([1.9705, 1.0308])
-        h, w, c = img.shape
+        h, w, c = tile.shape
 
         # reshape image
-        img = img.reshape((-1, 3))
+        tile = tile.reshape((-1, 3))
 
-        OD = -np.log((img.astype(float) + 1) / Io)
-
+        # calculate optical density
+        tile = tile.astype(float)
+        tile[tile == 0] = 1
+        OD = -np.log(tile / Io)
         ODhat = OD[~np.any(OD < beta, axis=1)]
-
         eigvals, eigvecs = np.linalg.eigh(np.cov(ODhat.T))
         That = ODhat.dot(eigvecs[:, 1:3])
 
@@ -29,8 +30,7 @@ def normalizeStaining(img, saveFile=None, Io=240, alpha=1, beta=0.15):
         vMin = eigvecs[:, 1:3].dot(np.array([(np.cos(minPhi), np.sin(minPhi))]).T)
         vMax = eigvecs[:, 1:3].dot(np.array([(np.cos(maxPhi), np.sin(maxPhi))]).T)
 
-        # a heuristic to make the vector corresponding to hematoxylin first and the
-        # one corresponding to eosin second
+        # heuristic to make hematoxylin first and eosin second
         if vMin[0] > vMax[0]:
             HE = np.array((vMin[:, 0], vMax[:, 0])).T
         else:
@@ -38,30 +38,15 @@ def normalizeStaining(img, saveFile=None, Io=240, alpha=1, beta=0.15):
 
         # rows correspond to channels (RGB), columns to OD values
         Y = np.reshape(OD, (-1, 3)).T
-
-        # determine concentrations of the individual stains
         C = np.linalg.lstsq(HE, Y, rcond=None)[0]
-
-        # normalize stain concentrations
         maxC = np.array([np.percentile(C[0, :], 99), np.percentile(C[1, :], 99)])
-        tmp = np.divide(maxC, maxCRef)
-        C2 = np.divide(C, tmp[:, np.newaxis])
-
-        Inorm = np.multiply(Io, np.exp(-HERef.dot(C2)))
-        Inorm[Inorm > 255] = 254
-        Inorm = np.reshape(Inorm.T, (h, w, 3)).astype(np.uint8)
-
-        # unmix hematoxylin and eosin
-        H = np.multiply(Io, np.exp(np.expand_dims(-HERef[:, 0], axis=1).dot(np.expand_dims(C2[0, :], axis=0))))
-        H[H > 255] = 254
-        H = np.reshape(H.T, (h, w, 3)).astype(np.uint8)
-
-        E = np.multiply(Io, np.exp(np.expand_dims(-HERef[:, 1], axis=1).dot(np.expand_dims(C2[1, :], axis=0))))
-        E[E > 255] = 254
-        E = np.reshape(E.T, (h, w, 3)).astype(np.uint8)
-        if saveFile is not None:
-            Image.fromarray(Inorm).save(saveFile)
+        tmp = np.divide(maxC, maxCRef).astype(np.float64)
+        C2 = np.divide(C, tmp[:, np.newaxis]).astype(np.float64)
+        with np.errstate(over='raise', divide='raise', invalid='raise'):
+            Inorm = np.multiply(Io, np.exp(-HERef.dot(C2)))
+            Inorm[Inorm > 255] = 254
+            Inorm = np.reshape(Inorm.T, (h, w, 3)).astype(np.uint8)
 
         return Inorm
-    except Exception as e:
-        print(e)
+    except (FloatingPointError, ValueError, np.linalg.LinAlgError):
+        return None
