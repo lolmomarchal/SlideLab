22e9bc822644decd1fda1e39cffc133d1fd44917
lolmomarchal <aolmomarchal@ucsd.edu>
2025-01-24 12:38:39 -0800

    fixed

--- SlideLab/SlidePreprocessing.py
+++ SlideLab/SlidePreprocessing.py
@@ -13,11 +13,12 @@ import cv2
 import time
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
+import random
 
 # classes/functions
 import Reports, SlideEncoding
 from TileNormalization import normalizeStaining, normalizeStaining_torch
-from TileQualityFilters import LaplaceFilter
+from TileQualityFilters import LaplaceFilter, plot_distribution
 from TissueMask import is_tissue, get_region_mask, TissueMask
 from tiling.TileIterator import TileIterator
 
@@ -86,8 +87,8 @@ def best_size(desired_mag, natural_mag, desired_size) -> int:
 
 def get_valid_coordinates(width, height, overlap, mask, size, scale, threshold):
     # example overlap of 2 and size of 256 = 128 stride
-    if overlap >1:
-        stride = size / overlap
+    if overlap > 1:
+        stride = size // overlap
     else:
         stride = size
     x_coords = np.arange(0, width - size + stride, stride)
@@ -132,13 +133,15 @@ def tile_slide_normalize_blurry_image(coord, desired_size, adjusted_size, patien
     tile_np = normalizeStaining(np.array(tile))
     if tile_np is None:
         return None
-    if not LaplaceFilter(tile_np):
+    blurry, var = LaplaceFilter(tile_np)
+    if not blurry:
+
         image_path = os.path.join(sample_path,
                                   f"{patient_id}_{coord[0]}_{coord[1]}_size_{desired_size}_mag_{desired_mag}.png")
         cv2.imwrite(image_path, tile_np[:, :, ::-1])
         return {"Patient_ID": patient_id, "x": coord[0], "y": coord[1], "tile_path": image_path,
-                "original_size": adjusted_size, "desired_size": desired_size, "desired_magnification": desired_mag}
-    return None
+                "original_size": adjusted_size, "desired_size": desired_size, "desired_magnification": desired_mag}, var
+    return None, var
 
 
 def normalize_gpu(coord, tile):
@@ -181,7 +184,8 @@ def save_tiles(coord, norm_tile, output_dir, patient_id, desired_size, desired_m
 def save_tiles_QC(coord, norm_tile, output_dir, patient_id, desired_size, desired_mag):
     try:
         norm_tile = norm_tile.numpy()
-        if not LaplaceFilter(norm_tile):
+        blurry, var = LaplaceFilter(norm_tile)
+        if not blurry:
             file_path = os.path.join(output_dir,
                                      f"{patient_id}_{coord[0]}_{coord[1]}_size_{desired_size}_mag_{desired_mag}.png")
             cv2.imwrite(file_path, norm_tile[:, :, ::-1])
@@ -192,7 +196,8 @@ def save_tiles_QC(coord, norm_tile, output_dir, patient_id, desired_size, desire
                 "tile_path": file_path,
                 "desired_size": desired_size,
                 "desired_magnification": desired_mag}
-            return metadata
+            return metadata, var
+        return None, var
     except Exception as e:
         print(f"Error in QC or saving tile: {e}")
         return None
@@ -217,6 +222,7 @@ def preprocessing(path, patient_id, args):
     # error and summary report
     error = []
     summary = []
+    vars = []
     # instance statistics
     total_tiles = -1
     valid_tiles = -1
@@ -232,15 +238,15 @@ def preprocessing(path, patient_id, args):
 
     def summary_():
         summary = [patient_id, path, total_tiles, valid_tiles, blurry_tiles,
-             time_mask_cpu, time_coordinates_cpu, time_patches_cpu, time_mask,
-             time_coordinates, time_patches]
+                   time_mask_cpu, time_coordinates_cpu, time_patches_cpu, time_mask,
+                   time_coordinates, time_patches]
         return summary
 
     # check 1: Opening Slide
     try:
         slide = openslide.OpenSlide(path)
-    except: # noqa: F821
-        error.append((patient_id, path, "Error Opening file" , "Slide Opening"))
+    except:  # noqa: F821
+        error.append((patient_id, path, "Error Opening file", "Slide Opening"))
         summary = summary_()
         summary.append("Error")
 
@@ -279,7 +285,7 @@ def preprocessing(path, patient_id, args):
     valid_tiles = valid_coordinates
 
     # setting up process information
-    if args.cpu_processes is None:
+    if args.cpu_processes is None or args.cpu_processes > os.cpu_count():
         max_workers = os.cpu_count()
     else:
         max_workers = args.cpu_processes
@@ -304,7 +310,7 @@ def preprocessing(path, patient_id, args):
                                      magnification=desired_magnification, adjusted_size=adjusted_size, overlap=overlap)
         with concurrent.futures.ThreadPoolExecutor(
                 max_workers=max_gpu_workers) as gpu_executor, concurrent.futures.ThreadPoolExecutor(
-                max_workers=max_workers) as cpu_executor:
+            max_workers=max_workers) as cpu_executor:
             gpu_futures = []
             cpu_futures = []
             for tile, coord in tile_iterator:
@@ -328,7 +334,12 @@ def preprocessing(path, patient_id, args):
                             cpu_executor.submit(save_tiles_QC, coord, norm_tile, tiles_dir, patient_id, desired_size,
                                                 desired_magnification))
             for future in concurrent.futures.as_completed(cpu_futures):
-                result = future.result()
+                # check if removing blurry
+                if args.normalize_staining and args.remove_blurry_tiles:
+                    result, var = future.result()
+                    vars.append(var)
+                elif args.normalize_staining and not args.remove_blurry_tiles:
+                    result = future.result()
                 if result is not None:
                     metadata_list.append(result)
 
@@ -349,7 +360,14 @@ def preprocessing(path, patient_id, args):
             elif args.normalize_staining and not args.remove_blurry_tiles:
                 results = list(executor.map(lambda p: tile_slide_normalize_image(*p), mult_args))
             else:
-                results = list(executor.map(lambda p: tile_slide_normalize_blurry_image(*p), mult_args))
+                results_ = list(executor.map(lambda p: tile_slide_normalize_blurry_image(*p), mult_args))
+                # need to rewrite -> should be a list of tuples
+                results = []
+                for item in results_:
+                    if item[0] is not None:
+                        results.append(item[0])
+                    vars.append(item[1])
+
         time_patches = time.time() - start_time_patches_user
         time_patches_cpu = time.process_time() - start_time_patches_cpu
         results_ = [result for result in results if result]
@@ -362,6 +380,38 @@ def preprocessing(path, patient_id, args):
         blurry_tiles = len(results_) if args.remove_blurry_tiles else None
     summary = summary_()
     summary.append("Processed")
+
+    # sanity check -> statistics for process do (1-2 examples)
+    if args.normalize_staining:
+        QC_path = os.path.join(sample_path, "QC_pipeline")
+        os.makedirs(QC_path, exist_ok=True)
+        # choose random coordinate
+        non_blurry_coords = list(zip(df_tiles['x'], df_tiles['y']))
+        random_coord = non_blurry_coords[random.randint(0, len(non_blurry_coords))]
+        region = slide.read_region((random_coord[0], random_coord[1]), 0, (adjusted_size, adjusted_size)).convert(
+            'RGB').resize(
+            (desired_size, desired_size), Image.BILINEAR)
+        region.save(os.path.join(QC_path, "original_non_blurry.png"))
+        normalized_img = Image.fromarray(normalizeStaining(np.array(region)))
+        normalized_img.save(os.path.join(QC_path, "normalized_non_blurry.png"))
+        if args.remove_blurry_tiles:
+            # laplacian distribution
+            distributions = os.path.join(QC_path, "tile_distributions")
+            os.makedirs(distributions, exist_ok=True)
+            var_path = os.path.join(distributions, "Laplacian_variance.png")
+            plot_distribution(vars, var_path, args.blur_threshold)
+            coordin = [tuple(coord) for coord in coordinates]
+
+            # also get an example for QC sample of a blurry and a non-blurry tile (valid coordinates)
+            different_coords = list(set(coordin) - set(non_blurry_coords))
+            if different_coords:
+                random_coord = different_coords[random.randint(0, len(different_coords))]
+                region = slide.read_region((random_coord[0], random_coord[1]), 0, (adjusted_size, adjusted_size)).convert(
+                    'RGB').resize(
+                    (desired_size, desired_size), Image.BILINEAR)
+                region.save(os.path.join(QC_path, "original_blurry.png"))
+                normalized_img = Image.fromarray(normalizeStaining(np.array(region)))
+                normalized_img.save(os.path.join(QC_path, "normalized_blurry.png"))
     return summary, error
 
 
@@ -465,13 +515,13 @@ def parse_args():
     # thresholds
     parser.add_argument("-th", "--tissue_threshold", type=float, default=0.7,
                         help="Threshold to consider a tile as Tissue(default: %(default)s)")
-    parser.add_argument("-bh", "--blur_threshold", type=int, default=0.015,
+    parser.add_argument("-bh", "--blur_threshold", type=float, default=0.015,
                         help="Threshold for laplace filter variance (default: %(default)s)")
 
     # for devices + multithreading
     parser.add_argument("--device", default=None)
-    parser.add_argument("--gpu_processes", default=1)
-    parser.add_argument("--cpu_processes", default=os.cpu_count())
+    parser.add_argument("--gpu_processes", type=int, default=1)
+    parser.add_argument("--cpu_processes", type=int, default=os.cpu_count())
 
     return parser.parse_args()
 
@@ -509,7 +559,7 @@ def main():
 
     # # process samples, will be multiprocessing the instances.
     # results = []
-    for i, row in tqdm.tqdm(patients.iterrows(), total = len(patients)):
+    for i, row in tqdm.tqdm(patients.iterrows(), total=len(patients)):
         print(f"Working on: {row['Patient ID']}")
         if not os.path.isfile(os.path.join(output_path, row["Patient ID"], row["Patient ID"] + ".csv")):
             results = preprocessing(row["Original Slide Path"], row["Patient ID"], args)
@@ -517,14 +567,13 @@ def main():
             # results.append(preprocessing(row["Original Slide Path"], row["Patient ID"], args))
     # encode after all patients have been preprocessed
     if args.encode:
-        for i, row in tqdm.tqdm(patients.iterrows(), total = len(patients)):
+        for i, row in tqdm.tqdm(patients.iterrows(), total=len(patients)):
             patient_id = row["Patient ID"]
             path = os.path.join(output_path, patient_id, patient_id + ".csv")
             if not os.path.isfile(os.path.join(encoder_path, str(patient_id) + ".h5")):
                 SlideEncoding.encode_tiles(patient_id, path, encoder_path, device)
 
-    # # rewrite patient_files
-    patient_files_encoded(patient_path)
+        patient_files_encoded(patient_path)
 
     # # write summary and error report
     # global summary, errors
--- SlideLab/TileQualityFilters.py
+++ SlideLab/TileQualityFilters.py
@@ -1,5 +1,8 @@
 import cv2
 from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 # laplace filter to discard blurry images
@@ -7,8 +10,20 @@ def LaplaceFilter(img, var_threshold=0.015):
     # turn into gray
     grayscale = rgb2gray(img)
     laplace_img = cv2.Laplacian(grayscale, cv2.CV_64F, ksize=3)
-    if laplace_img.var() >= var_threshold:
-        return True
-    return False
+    variance = laplace_img.var()
+    if variance <= var_threshold:
+        return True, variance
+    return False, variance
+
+
+def plot_distribution(values, save_path, var_threshold = 0.015):
+    plt.figure(figsize = (6,6))
+    distribution = pd.Series(values, name = "Laplacian Variance")
+    sns.histplot(data = distribution, kde = True)
+    plt.axvline(x= var_threshold, color = "red", linestyle = "--", label = f"threshold = {var_threshold}")
+    plt.title("Laplacian variance distribution")
+    plt.ylabel("Count")
+    plt.legend()
+    plt.savefig(save_path)
 
 
