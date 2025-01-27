e66a609cc20510d8bf4502c595cfe863ce02beb8
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2025-01-26 17:59:06 -0800

    Update TileNormalization.py

--- SlideLab/TileNormalization.py
+++ SlideLab/TileNormalization.py
@@ -112,7 +112,7 @@ def normalizeStaining_torch(tile, Io=240, alpha=1, beta=0.15, device=None):
         Inorm = torch.clamp(Inorm, 0, 255)
         Inorm = Inorm.T.reshape(h, w, 3)
 
-        return Inorm.byte().to('cpu').numpy()
+        return Inorm.byte().to('cpu')
 
     except (RuntimeError, ValueError, FloatingPointError):
         return None
