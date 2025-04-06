51fa0c1406d86e5b248d3cc96c4e5686a97bf4dd
Lorenzo Olmo Marchal <114376800+lolmomarchal@users.noreply.github.com>
2025-04-05 21:25:00 -0700

    Update SlidePreprocessing_new.py

--- SlideLab/SlidePreprocessing_new.py
+++ SlideLab/SlidePreprocessing_new.py
@@ -293,7 +293,7 @@ def preprocessing(path, patient_id, args):
         )
         
     
-        num_gpu_workers = max_workers // 2 
+        num_gpu_workers = max_workers // 3
         num_saving_workers = max_workers - num_gpu_workers  
         
         from torch.utils.data import DataLoader
@@ -671,30 +671,10 @@ def main():
         print("---------------------------------")
         print(f"Working on: {row['Patient ID']}")
         if not os.path.isfile(os.path.join(output_path, row["Patient ID"], row["Patient ID"] + ".csv")):
-            print("starting preprocessing")
+          
             results = preprocessing(row["Original Slide Path"], row["Patient ID"], args)
-            print(f"Summary: {results[0]}, Errors: {results[1]}")
-            print("Getting Reports ...")
+
             Reports.Reports([results[0]], [results[1]], output_path)
-            if torch.cuda.is_available():
-                    print(" GPU Memory Usage:")
-                    print(f"Allocated: {round(torch.cuda.memory_allocated(0)/1024**3, 1)} GB")
-                    print(f"Reserved: {round(torch.cuda.memory_reserved(0)/1024**3, 1)} GB")
-            process = psutil.Process(os.getpid())
-            mem_info = process.memory_info()
-            
-            print("\nCPU Statistics:")
-            print(f"RSS (Resident Set Size): {mem_info.rss / 1024 ** 2:.2f} MB")
-            print(f"VMS (Virtual Memory Size): {mem_info.vms / 1024 ** 2:.2f} MB")
-            print(f"CPU Usage: {psutil.cpu_percent(interval=1)}%")
-            
-            gc.collect()
-            after_gc_mem = process.memory_info().rss / 1024 ** 2
-            print(f"Memory usage after garbage collection: {after_gc_mem:.2f} MB")
-            print("---------------------------------")
-            
-                
-            # results.append(preprocessing(row["Original Slide Path"], row["Patient ID"], args))
 
     # encode after all patients have been preprocessed
     encoding_times = []
